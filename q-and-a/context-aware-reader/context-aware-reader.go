0bd8e537417250bda4ee5fcb2de136fd1202db45
Gregor Pifko <gregor.pifko@gmail.com>
2026-08-14 05:24:27 +0200

    feat(context-aware-reader): stop reading when context is cancelled

---
+++ q-and-a/context-aware-reader/context-aware-reader.go
@@ -0,0 +1,25 @@
+package contextawarereader
+
+import (
+	"context"
+	"io"
+)
+
+func NewCancellableReader(ctx context.Context, rdr io.Reader) io.Reader {
+	return &readerCtx{
+		ctx:      ctx,
+		delegate: rdr,
+	}
+}
+
+type readerCtx struct {
+	ctx      context.Context
+	delegate io.Reader
+}
+
+func (r *readerCtx) Read(p []byte) (n int, err error) {
+	if err := r.ctx.Err(); err != nil {
+		return 0, err
+	}
+	return r.delegate.Read(p)
+}
---
+++ q-and-a/context-aware-reader/context-aware-reader_test.go
@@ -0,0 +1,61 @@
+package contextawarereader_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	contextawarereader "github.com/gregor-pifko/learn-go-with-tests/q-and-a/context-aware-reader"
+)
+
+func TestContextAwareReader(t *testing.T) {
+	t.Run("behaves lika a normal reader", func(t *testing.T) {
+		rdr := contextawarereader.NewCancellableReader(context.Background(), strings.NewReader("123456"))
+		got := make([]byte, 3)
+		_, err := rdr.Read(got)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "123")
+
+		_, err = rdr.Read(got)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "456")
+	})
+
+	t.Run("stops reading when cancelled", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		rdr := contextawarereader.NewCancellableReader(ctx, strings.NewReader("123456"))
+		got := make([]byte, 3)
+		_, err := rdr.Read(got)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertBufferHas(t, got, "123")
+
+		cancel()
+
+		n, err := rdr.Read(got)
+
+		if err == nil {
+			t.Error("expected an error after cancellation but didn't get one")
+		}
+
+		if n > 0 {
+			t.Errorf("expected 0 bytes to be read after cancellation but %d were read", n)
+		}
+	})
+}
+
+func assertBufferHas(t *testing.T, buf []byte, want string) {
+	t.Helper()
+	got := string(buf)
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
